b5e00a67e88e764545ee2d71b9f7c6db0a45b1aa
Snehal Nair <snehal_nair@moderncoe.edu.in>
2022-05-07 21:57:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,31 @@ footer {padding: 0px 10px;
 	padding-top:10px;
 }
 
+know = {
+
+"Hello" : "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Hi there!",
+"hello" : "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Hi there!",
+"HELLO" : "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Hi there!",
+"Who are you?" : "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp I am Nikhil's Assistant",
+"How are you?" : "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp I am Fine",
+"How old are you?" : "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp I am infinity in age"
+
+
+
+
+};
+
+function talk() {
+var user = document.getElementById("userBox").value;
+document.getElementById("userBox").value= "";
+document.getElementById("chatLog").innerHTML += user+"<br>";
+
+if (user in know) {
+document.getElementById("chatLog").innerHTML += know[user] + "<br>";
+} else 
+
+{
+document.getElementById("chatLog").innerHTML += "I don't understand... <br>";
+}
+}
+
